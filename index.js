a53c81bd613b2791a1e2addbc95a517fda6bd924
Sashko Stubailo <sashko@stubailo.com>
2015-06-25 18:20:41 -0700

    Code style

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-'use strict'
+'use strict';
 
-var detective = require('detective')
-var patch = require('patch-text')
-var hasRequire = require('has-require')
-var extend = require('xtend')
-var mapObject = require('map-obj')
+var detective = require('detective');
+var patch = require('patch-text');
+var hasRequire = require('has-require');
+var extend = require('xtend');
+var mapObject = require('map-obj');
 
 // replacements in the form:
 // moduleName: "RequireFunc"
 // replaces all occurences of require("moduleName*") with
 // RequireFunc("moduleName*")
 module.exports = function externalize (code, replacements) {
-  return patch(code, detective
-    .find(code, {nodes: true})
-    .nodes
+  var patchRanges = detective.find(code, {nodes: true}).nodes
     .filter(requireLiteral)
     .map(function (node) {
       var requireString = node.arguments[0].value.toString();
       var replaceRequireFunc = null;
+
       mapObject(replacements, function (moduleName, requireFunc) {
         if (requireString === moduleName ||
             requireString.indexOf(moduleName + "/") === 0) {
           replaceRequireFunc = requireFunc;
         }
+
         return requireFunc;
       });
 
@@ -36,11 +36,12 @@ module.exports = function externalize (code, replacements) {
     })
     .filter(function (node) {
       return node.replacement != null
-    }))
+    });
 
+  return patch(code, patchRanges);
 }
 
 function requireLiteral (node) {
-  var arg = node.arguments[0]
-  return arg && arg.type === 'Literal'
+  var arg = node.arguments[0];
+  return arg && arg.type === 'Literal';
 }
--- test.js
+++ test.js
@@ -7,8 +7,6 @@ test(function tests (t) {
   t.equal(replace('require("foo")', {foo: 'bar'}), 'bar("foo")')
   t.equal(replace('require("foo");require("bar")', {foo: 'bar', bar: 'baz'}), 'bar("foo");baz("bar")')
   t.equal(replace('require("foo")', {}), 'require("foo")')
-
   t.equal(replace('require("foo/bar")', {'foo/bar': 'baz'}), 'baz("foo/bar")')
-
   t.end()
 })
